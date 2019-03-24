f01c63b8d802e8e63e7ed932dc01c887631d4520
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2019-03-24 22:31:12 +0700

    Initial Commit

---
+++ EnvSensors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnvSensors
+//
+//  Created by Vladimir Lagunov on 24/03/2019.
+//  Copyright © 2019 Vladimir Lagunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
