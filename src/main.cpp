142b735ece8f9b732c25710761f9d91e2b4c0378
Vladimir Lagunov <vladimir.lagunov@jetbrains.com>
2019-04-29 00:43:33 +0700

    Enabled notifications in gatt service

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,94 @@
 
 #include <ble/BLE.h>
 #include <ble/Gap.h>
-#include <ble/services/EnvironmentalService.h>
+//#include <ble/services/EnvironmentalService.h>
+#include <ble/GattCharacteristic.h>
 #include <mbed.h>
 #include <BME280.h>
 
-
 #include <nrf_soc.h>
 
+/**
+* @class EnvironmentalService
+* @brief BLE Environmental Service. This service provides temperature, humidity and pressure measurement.
+* Service:  https://developer.bluetooth.org/gatt/services/Pages/ServiceViewer.aspx?u=org.bluetooth.service.environmental_sensing.xml
+* Temperature: https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.temperature.xml
+* Humidity: https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.humidity.xml
+* Pressure: https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.pressure.xml
+*/
+class EnvironmentalService {
+public:
+    typedef int16_t TemperatureType_t;
+    typedef uint16_t HumidityType_t;
+    typedef uint32_t PressureType_t;
+
+    /**
+     * @brief   EnvironmentalService constructor.
+     * @param   _ble Reference to BLE device.
+     */
+    EnvironmentalService(BLE &_ble) :
+            ble(_ble),
+            temperatureCharacteristic(GattCharacteristic::UUID_TEMPERATURE_CHAR, &temperature,
+                                      GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY),
+            humidityCharacteristic(GattCharacteristic::UUID_HUMIDITY_CHAR, &humidity,
+                                   GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY),
+            pressureCharacteristic(GattCharacteristic::UUID_PRESSURE_CHAR, &pressure,
+                                   GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY) {
+        static bool serviceAdded = false; /* We should only ever need to add the information service once. */
+        if (serviceAdded) {
+            return;
+        }
+
+        GattCharacteristic *charTable[] = {&humidityCharacteristic,
+                                           &pressureCharacteristic,
+                                           &temperatureCharacteristic};
+
+        GattService environmentalService(GattService::UUID_ENVIRONMENTAL_SERVICE, charTable,
+                                         sizeof(charTable) / sizeof(GattCharacteristic *));
+
+        ble.gattServer().addService(environmentalService);
+        serviceAdded = true;
+    }
+
+    /**
+     * @brief   Update humidity characteristic.
+     * @param   newHumidityVal New humidity measurement.
+     */
+    void updateHumidity(HumidityType_t newHumidityVal) {
+        humidity = (HumidityType_t) (newHumidityVal * 100);
+        ble.gattServer().write(humidityCharacteristic.getValueHandle(), (uint8_t *) &humidity, sizeof(HumidityType_t));
+    }
+
+    /**
+     * @brief   Update pressure characteristic.
+     * @param   newPressureVal New pressure measurement.
+     */
+    void updatePressure(PressureType_t newPressureVal) {
+        pressure = (PressureType_t) (newPressureVal * 10);
+        ble.gattServer().write(pressureCharacteristic.getValueHandle(), (uint8_t *) &pressure, sizeof(PressureType_t));
+    }
+
+    /**
+     * @brief   Update temperature characteristic.
+     * @param   newTemperatureVal New temperature measurement.
+     */
+    void updateTemperature(float newTemperatureVal) {
+        temperature = (TemperatureType_t) (newTemperatureVal * 100);
+        ble.gattServer().write(temperatureCharacteristic.getValueHandle(), (uint8_t *) &temperature,
+                               sizeof(TemperatureType_t));
+    }
+
+private:
+    BLE &ble;
+
+    TemperatureType_t temperature;
+    HumidityType_t humidity;
+    PressureType_t pressure;
+
+    ReadOnlyGattCharacteristic<TemperatureType_t> temperatureCharacteristic;
+    ReadOnlyGattCharacteristic<HumidityType_t> humidityCharacteristic;
+    ReadOnlyGattCharacteristic<PressureType_t> pressureCharacteristic;
+};
 
 class App {
     EventQueue eventQueue{50 * EVENTS_EVENT_SIZE};
@@ -36,6 +117,10 @@ class App {
         std::cerr << "Someone connected" << std::endl;
     }
 
+    void onHVX(const GattHVXCallbackParams *params) {
+        std::cerr << "onHVX" << std::endl;
+    }
+
 public:
     int run();
 };
@@ -57,6 +142,7 @@ void App::bleInitComplete(BLE::InitializationCompleteCallbackContext *context) {
     }
 
     ble.onEventsToProcess({this, &App::scheduleBleEventProcessing});
+    ble.gattClient().onHVX({this, &App::onHVX});
 
     environmentalService = std::make_unique<EnvironmentalService>(ble);
 
@@ -108,11 +194,12 @@ void App::measureBme280() {
             << "Pressure:    " << pressure << " hPa" << std::endl
             << "Humidity:    " << humidity << "%" << std::endl;
 
-    if (bluetooth.gap().getState().connected) {
+    Gap &gap = bluetooth.gap();
+    if (gap.getState().connected) {
+        std::cerr << "Gap is connected" << std::endl;
         environmentalService->updateTemperature(temperature);
         environmentalService->updatePressure((uint32_t) pressure);
         environmentalService->updateHumidity((uint16_t) humidity);
-        std::cerr << "Gap is connected" << std::endl;
     } else {
         std::cerr << "Gap is not connected" << std::endl;
     }
